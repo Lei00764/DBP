9a85c10a0ecc0c9b2647274e63dc1c29a48c836f
lei <xiang.lei.se@foxmail.com>
2023-07-04 16:30:18 +0800

    完成注册功能

--- database-back-web/Controllers/LoginController.cs
+++ database-back-web/Controllers/LoginController.cs
@@ -4,19 +4,20 @@ using Microsoft.EntityFrameworkCore;
 
 [ApiController]
 [Route("api/[controller]")]  // RESTful 风格
-public class loginController : ControllerBase  // 命名规范，继承自 ControllerBase 类的类名必须与 Controller 结尾
+public class LoginController : ControllerBase  // 命名规范，继承自 ControllerBase 类的类名必须与 Controller 结尾
 {
 
     private readonly AppDbContext _database;
 
-    public loginController(AppDbContext appDbContext)
+    public LoginController(AppDbContext appDbContext)
     {
-        _database = appDbContext;
+        _database = appDbContext;  // 依赖注入，在整个类中使用它来进行数据库操作。
     }
 
     // http://localhost:5045/api/login?id=1&password=xxx
+    // 定义一个名为 loginController 的控制器类，该类继承自 ControllerBase 类，这样它就可以处理 HTTP 请求了
     [HttpGet]
-    public IActionResult CheckUser(string email, string password)  // 对外暴露的接口参数名为 id 和 password
+    public async Task<IActionResult> CheckUserAsync(string email, string password)  // 对外暴露的接口参数名为 id 和 password
     {
         var type = -1;  // 区分登陆的是管理员还是用户
         // type=1 用户
@@ -24,8 +25,9 @@ public class loginController : ControllerBase  // 命名规范，继承自 Contr
 
         var code = 400;
         var msg = "success";
-        object user_data = _database.Users.ToListAsync().Result;
-        object admin_data = _database.Administrators.ToListAsync().Result;
+        var user_data = await _database.Users.ToListAsync();
+        var admin_data = await _database.Administrators.ToListAsync();
+
         // 如果数据库中没有数据，返回错误信息
         if (user_data == null && admin_data == null)
         {
@@ -39,21 +41,25 @@ public class loginController : ControllerBase  // 命名规范，继承自 Contr
         }
         // 遍历data，找到id和password匹配的用户或管理员
         bool temp = false;
-        foreach (var user in (System.Collections.Generic.List<auth.Models.User>)user_data)
+        if (user_data != null)
         {
-            if (user.Email == email && user.PassWord == password)
+            foreach (var user in user_data)
             {
-                code = 200;
-                msg = "普通用户登录成功";
-                type = 1;
-                temp = true;
-                break;
-            }
+                if (user.Email == email && user.PassWord == password)
+                {
+                    code = 200;
+                    msg = "普通用户登录成功";
+                    type = 1;
+                    temp = true;
+                    break;
+                }
 
+            }
         }
+
         if (temp == false)//用户表里未找到
         {
-            foreach (var admin in (System.Collections.Generic.List<auth.Models.Administrator>)admin_data)
+            foreach (var admin in admin_data)
             {
                 if (admin.Email == email && admin.PassWord == password)
                 {
@@ -68,8 +74,6 @@ public class loginController : ControllerBase  // 命名规范，继承自 Contr
                     code = 400;
                     msg = "用户名或密码错误";
                 }
-                // 打印
-                //System.Console.WriteLine(admin.UserId);
             }
         }
         return Ok(new
--- database-back-web/Models/User.cs
+++ database-back-web/Models/User.cs
@@ -9,25 +9,25 @@ public class User
     // 请与数据库中表字段保持一致
     [Key]
     [Column("USERID")]
-    public int UserId { get; set; }
+    public int? UserId { get; set; }
     [Column("USERNAME")]
     public string? UserName { get; set; }
     [Column("PASSWORD")]
     public string? PassWord { get; set; }
     [Column("AVATAR")]
-    public string? Avatar {get;set;}
+    public string? Avatar { get; set; }
     [Column("PROFESSIONAL")]
-    public string? Professional {get;set;}
+    public string? Professional { get; set; }
     [Column("SIGNATURE")]
-    public string? Signature {get;set;}
+    public string? Signature { get; set; }
     [Column("EMAIL")]
-    public string? Email {get;set;}
+    public string? Email { get; set; }
     [Column("TEL")]
-    public string? Tel {get;set;}
+    public string? Tel { get; set; }
     [Column("FOLLOWERNUM")]
-    public int FollowerNum {get;set;}
+    public int? FollowerNum { get; set; }
     [Column("THEMEID")]
-    public int ThemeID {get;set;}
-    
+    public int? ThemeID { get; set; }
+
 
 }
