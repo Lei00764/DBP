897054dcbb08bc5b19a02e0d223a13cdc76f5318
Li_Zekai <1309218780@qq.com>
2023-07-06 17:07:47 +0800

    根据文章id显示文章详情

--- database-back-web/Controllers/ArticleController.cs
+++ database-back-web/Controllers/ArticleController.cs
@@ -4,17 +4,17 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 
 [ApiController]
-[Route("api/forum/[controller]")]  // RESTful 风格
-public class LoadArticleController : ControllerBase  // 命名规范，继承自 ControllerBase 类的类名必须与 Controller 结尾
+[Route("api/[controller]")]  // RESTful 风格
+public class ArticleController : ControllerBase  // 命名规范，继承自 ControllerBase 类的类名必须与 Controller 结尾
 {
-    private readonly AppDbContext _database;
+    private AppDbContext _database;
 
-    public LoadArticleController(AppDbContext appDbContext)
+    public ArticleController(AppDbContext appDbContext)
     {
-        _database = appDbContext;  // 依赖注入，在整个类中使用它来进行数据库操作。
+        _database = appDbContext;  // 依赖注入，在整个类中使用它来进行数据库操作
     }
 
-    [HttpGet]
+    [HttpGet("loadArticle")]
     public async Task<IActionResult> GetArticleByTagAsync(string tag)
     {
         var code = 400;
@@ -59,4 +59,75 @@ public class LoadArticleController : ControllerBase  // 命名规范，继承自
             data = article_data
         });
     }
+
+    [HttpGet("viewArticle/{article_id}")]
+    public async Task<IActionResult> GetArticleDetailsAsync(int article_id)
+    {
+        var code = 200;
+        var msg = "success";
+        var temp=await _database.Articles.ToListAsync();
+        bool exist=false;
+        if(temp!=null)//判断表内是否有改文章
+        {
+        foreach (var article in temp)
+            {
+               if(article.PostId==article_id)
+               {
+                exist=true;
+                break;
+               }
+            }
+        }
+        if(exist){
+        var article_data = await _database.Articles
+            .Where(a => a.PostId == article_id)
+            .Join(_database.Users,
+                article => article.AuthorId,
+                user => user.UserId,
+                (article, user) => new
+                {
+                    ID=article.PostId,//文章ID
+                    TAG = article.Tag,  // 文章标签
+                    Title = article.Title,  // 文章标题
+                    Views = article.Views,  // 文章浏览量
+                    FavouriteNum = article.FavouriteNum,  // 文章收藏量
+                    LikeNum = article.LikeNum,  // 文章点赞量
+                    AuthorName = user.UserName, // 包含作者的名字
+                    Content = article.Content,  // 文章内容
+                    IsBanned=article.IsBanned
+                })
+            .ToListAsync();
+        
+        if(article_data[0].IsBanned!=0)
+        {
+            code=400;
+            msg="该文章已被封禁";
+            return BadRequest(new
+            {
+                code=code,
+                msg=msg
+            });
+        }
+        return Ok(new
+        {
+            code = code,
+            msg = msg,
+            data=article_data
+
+
+        });
+        }
+        else
+        {
+            code=400;
+            msg="不存在该文章";
+            return BadRequest(new
+            {
+                code=code,
+                msg=msg
+            });
+        }
+        //!!!!!!TODO：更新Views!!!!!!!!!
+    }
+
 }
