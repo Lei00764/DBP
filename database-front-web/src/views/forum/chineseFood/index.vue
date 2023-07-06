4013c89daf02d662a705467dda9574681babc86a
RoxyMigurdia ~ <2046635721@qq.com>
2023-07-06 16:09:23 +0800

    论坛界面更新

--- database-front-web/src/views/forum/chineseFood/index.vue
+++ database-front-web/src/views/forum/chineseFood/index.vue
@@ -45,25 +45,31 @@
                     </el-form-item>
 
                     <el-form-item>
-                        <el-button type="primary" class="forum-button1" @click="doChineseFood">
+                        <el-button type="primary" class="forum-button1" @click="doWesternFood">
+                            <span>全部</span>
+                        </el-button>
+                    </el-form-item>
+
+                    <el-form-item>
+                        <el-button type="primary" class="forum-button2" @click="doChineseFood">
                             <span>中餐</span>
                         </el-button>
                     </el-form-item>
 
                     <el-form-item>
-                        <el-button type="primary" class="forum-button2" @click="doWesternFood">
+                        <el-button type="primary" class="forum-button3" @click="doWesternFood">
                             <span>西餐</span>
                         </el-button>
                     </el-form-item>
 
                     <el-form-item>
-                        <el-button type="primary" class="forum-button3" @click="doDessert">
+                        <el-button type="primary" class="forum-button4" @click="doDessert">
                             <span>甜点</span>
                         </el-button>
                     </el-form-item>
 
                     <el-form-item>
-                        <el-button type="primary" class="forum-button4" @click="doOthers">
+                        <el-button type="primary" class="forum-button5" @click="doOthers">
                             <span>其他</span>
                         </el-button>
                     </el-form-item>
@@ -232,7 +238,7 @@ const formData = reactive({
     top: 200px;
     left: 100px;
     height: 50px;
-    width: 250px;
+    width: 210px;
     background: #08664B;
     border-color: rgb(255, 255, 255);
     border-radius: 30px;
@@ -249,9 +255,9 @@ const formData = reactive({
     /*菜品种类第二个按钮相关设置 */
     position: absolute;
     top: 200px;
-    left: 420px;
+    left: 360px;
     height: 50px;
-    width: 250px;
+    width: 210px;
     background: #08664B;
     border-color: rgb(255, 255, 255);
     border-radius: 30px;
@@ -263,13 +269,14 @@ const formData = reactive({
     color: #08664B;
     border-color: rgb(255, 255, 255);
 }
+
 .forum-button3 {
     /*菜品种类第三个按钮相关设置 */
     position: absolute;
     top: 200px;
-    left: 740px;
+    left: 620px;
     height: 50px;
-    width: 250px;
+    width: 210px;
     background: #08664B;
     border-color: rgb(255, 255, 255);
     border-radius: 30px;
@@ -281,20 +288,38 @@ const formData = reactive({
     color: #08664B;
     border-color: rgb(255, 255, 255);
 }
-
 .forum-button4 {
     /*菜品种类第四个按钮相关设置 */
     position: absolute;
     top: 200px;
-    left: 1060px;
+    left: 880px;
     height: 50px;
-    width: 250px;
+    width: 210px;
     background: #08664B;
     border-color: rgb(255, 255, 255);
     border-radius: 30px;
 }
 
 .forum-button4:hover {
+    border-radius: 30px;
+    background-color: #f8b720;
+    color: #08664B;
+    border-color: rgb(255, 255, 255);
+}
+
+.forum-button5 {
+    /*菜品种类第五个按钮相关设置 */
+    position: absolute;
+    top: 200px;
+    left: 1140px;
+    height: 50px;
+    width: 210px;
+    background: #08664B;
+    border-color: rgb(255, 255, 255);
+    border-radius: 30px;
+}
+
+.forum-button5:hover {
     border-radius: 30px;
     background-color: #f8b720;
     color: #08664B;
