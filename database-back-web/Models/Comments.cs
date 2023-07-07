68acc3ff266d389a84050d78351ed18d7d98ee56
lei <xiang.lei.se@foxmail.com>
2023-07-07 16:04:00 +0800

    添加 Comment.cs Model

---
+++ database-back-web/Models/Comments.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;  // for Key
+using System.ComponentModel.DataAnnotations.Schema;  // for Column
+
+namespace auth.Models;
+
+[Table("COMMENTS")]
+public class Comment
+{
+    [Key]
+    [Column("MSGID")]
+    public int? MsgId { get; set; }
+
+    [Column("USERID")]
+    public int? UserId { get; set; }
+
+    [Column("POSTID")]
+    public int? PostId { get; set; }
+
+    [Column("RELEASETIME")]
+    public DateTime? ReleaseTime { get; set; }
+
+    [Column("CONTENT")]
+    public string? Content { get; set; }
+
+    [Column("ISBANNED")]
+    public int? IsBanned { get; set; }
+}
