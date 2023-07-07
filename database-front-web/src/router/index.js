4eabbe92f24e209a57c24b54012dc44b125f2053
Duck800 <1296534587@qq.com>
2023-07-07 15:29:00 +0800

    新增审核专业认证页面

--- database-front-web/src/router/index.js
+++ database-front-web/src/router/index.js
@@ -30,6 +30,11 @@ const router = createRouter({
             component: () => import('@/views/homeAdmin/index.vue'),
         },
         {
+            path: '/checkProfession',  // 审核专业认证
+            name: 'checkProfession',
+            component: () => import('@/views/checkProfession/index.vue'),
+        },
+        {
             path: '/announcement',  // 公告
             name: 'announcement',
             component: () => import('@/views/announcement/index.vue'),
