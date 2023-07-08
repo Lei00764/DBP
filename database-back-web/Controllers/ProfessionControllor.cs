0480b1059ba782b6276bbd3f27efd3c52aa72725
Li_Zekai <1309218780@qq.com>
2023-07-08 21:51:33 +0800

    增加了管理员查看专业认证申请的api

--- database-back-web/Controllers/ProfessionControllor.cs
+++ database-back-web/Controllers/ProfessionControllor.cs
@@ -16,24 +16,25 @@ public class ProfessionController : ControllerBase
         _database = appDbContext;  // 依赖注入，在整个类中使用它来进行数据库操作
     }
 
-    [HttpPost("Proffesion")]//提交专业认证申请（用户）
+    [HttpPost("ApplyForProffesion")]//提交专业认证申请（用户）
      public async Task<IActionResult> ApplyForAsync(int user_id,int year, int month, int day,string illustrate,string evidence)//提交专业认证申请（用户）
      {
         var code = 200;
         var msg = "申请已提交";
-        var user_data = await _database.Users.ToListAsync();
+        // var request = await _database.Users.ToListAsync();
         bool exist=false;
-        //time=(2020,7,4)
-        if (user_data == null)
-        {
-            code = 400;
-            msg = "用户不存在";
-            return BadRequest(new
-            {
-                code = code,
-                msg = msg,
-            });
-        }
+        // //time=(2020,7,4)
+        // if (user_data == null)
+        // {
+        //     code = 400;
+        //     msg = "用户不存在";
+        //     return BadRequest(new
+        //     {
+        //         code = code,
+        //         msg = msg,
+        //     });
+        // }
+        
         var u=_database.Users.Where(x=>x.UserId==user_id);
         foreach (var item in u)
         {
@@ -53,6 +54,20 @@ public class ProfessionController : ControllerBase
                 msg = msg,
             });
         }
+        // var record= _database.Professions.Where(x=>x.UserId==user_id);
+        // foreach (var r in record)
+        // {
+        //     if (r.UserId == user_id)
+        //     {
+        //     code = 400;
+        //     msg = "请勿重复提交";
+        //     return BadRequest(new
+        //     {
+        //         code = code,
+        //         msg = msg,
+        //     });
+        //     }
+        // }
         var newRecord = new Profession()
         {
             UserId = user_id,
@@ -70,4 +85,55 @@ public class ProfessionController : ControllerBase
         });
 
     }
+
+    [HttpGet("ViewRequest/{request_id}")]
+     public async Task<IActionResult> ViewRequest(int request_id)//查看认证申请（管理员）
+     {
+        var code = 200;
+        var msg = "success";
+        bool exist=false;
+        string? illustrate="";
+        string? evidence="";
+
+        // var r_data=await _database.Professions.ToListAsync();
+        // if(r_data==null)
+        // {
+        //     code = 400;
+        //     msg = "用户不存在";
+        //     return BadRequest(new
+        //     {
+        //         code = code,
+        //         msg = msg,
+        //     });
+        // }
+        var record= _database.Professions.Where(x=>x.RequestId==request_id);
+        foreach (var r in record)
+        {
+            if (r.RequestId == request_id)
+            {
+            exist=true;
+            illustrate=r.Illustrate;
+            evidence=r.Evidence;
+            break;
+            }
+        }
+        if(exist==false)
+        {
+            code = 400;
+            msg = "认证申请不存在";
+            return BadRequest(new
+            {
+                code = code,
+                msg = msg,
+            });            
+        }
+
+        return Ok(new
+        {
+            code = code,
+            msg = msg,
+            illustrate=illustrate,
+            evidence=evidence
+        }); 
+     }
 }
