177c184918d470c2a50d84bd0aeffc5dd7e07cfb
Li_Zekai <1309218780@qq.com>
2023-07-09 10:09:58 +0800

    增加管理员处理专业认证的api

--- database-back-web/Controllers/ProfessionControllor.cs
+++ database-back-web/Controllers/ProfessionControllor.cs
@@ -17,34 +17,21 @@ public class ProfessionController : ControllerBase
     }
 
     [HttpPost("ApplyForProffesion")]//提交专业认证申请（用户）
-     public async Task<IActionResult> ApplyForAsync(int user_id,int year, int month, int day,string illustrate,string evidence)//提交专业认证申请（用户）
-     {
+    public async Task<IActionResult> ApplyForAsync(int user_id, int year, int month, int day, string illustrate, string evidence)//提交专业认证申请（用户）
+    {
         var code = 200;
         var msg = "申请已提交";
-        // var request = await _database.Users.ToListAsync();
-        bool exist=false;
-        // //time=(2020,7,4)
-        // if (user_data == null)
-        // {
-        //     code = 400;
-        //     msg = "用户不存在";
-        //     return BadRequest(new
-        //     {
-        //         code = code,
-        //         msg = msg,
-        //     });
-        // }
-        
-        var u=_database.Users.Where(x=>x.UserId==user_id);
+        bool exist = false;
+        var u = _database.Users.Where(x => x.UserId == user_id);
         foreach (var item in u)
         {
-            if (item.UserId== user_id)
+            if (item.UserId == user_id)
             {
-                exist=true;
+                exist = true;
                 break;
             }
         }
-        if(exist==false)
+        if (exist == false)
         {
             code = 400;
             msg = "用户不存在";
@@ -54,20 +41,6 @@ public class ProfessionController : ControllerBase
                 msg = msg,
             });
         }
-        // var record= _database.Professions.Where(x=>x.UserId==user_id);
-        // foreach (var r in record)
-        // {
-        //     if (r.UserId == user_id)
-        //     {
-        //     code = 400;
-        //     msg = "请勿重复提交";
-        //     return BadRequest(new
-        //     {
-        //         code = code,
-        //         msg = msg,
-        //     });
-        //     }
-        // }
         var newRecord = new Profession()
         {
             UserId = user_id,
@@ -87,37 +60,25 @@ public class ProfessionController : ControllerBase
     }
 
     [HttpGet("ViewRequest/{request_id}")]
-     public async Task<IActionResult> ViewRequest(int request_id)//查看认证申请（管理员）
-     {
+    public async Task<IActionResult> ViewRequest(int request_id)//查看认证申请（管理员）
+    {
         var code = 200;
         var msg = "success";
-        bool exist=false;
-        string? illustrate="";
-        string? evidence="";
-
-        // var r_data=await _database.Professions.ToListAsync();
-        // if(r_data==null)
-        // {
-        //     code = 400;
-        //     msg = "用户不存在";
-        //     return BadRequest(new
-        //     {
-        //         code = code,
-        //         msg = msg,
-        //     });
-        // }
-        var record= _database.Professions.Where(x=>x.RequestId==request_id);
+        bool exist = false;
+        string? illustrate = "";
+        string? evidence = "";
+        var record = _database.Professions.Where(x => x.RequestId == request_id);
         foreach (var r in record)
         {
             if (r.RequestId == request_id)
             {
-            exist=true;
-            illustrate=r.Illustrate;
-            evidence=r.Evidence;
-            break;
+                exist = true;
+                illustrate = r.Illustrate;
+                evidence = r.Evidence;
+                break;
             }
         }
-        if(exist==false)
+        if (exist == false)
         {
             code = 400;
             msg = "认证申请不存在";
@@ -125,15 +86,85 @@ public class ProfessionController : ControllerBase
             {
                 code = code,
                 msg = msg,
-            });            
+            });
         }
 
         return Ok(new
         {
             code = code,
             msg = msg,
-            illustrate=illustrate,
-            evidence=evidence
-        }); 
-     }
+            illustrate = illustrate,
+            evidence = evidence
+        });
+    }
+
+    [HttpPost("DealRequest")]
+    public async Task<IActionResult> DealRequestAync(int request_id, int response)//处理认证申请（管理员）response=1为通过2为不通过
+    {
+        var code = 200;
+        var msg = "success";
+        bool exist = false;
+        var record = _database.Professions.Where(x => x.RequestId == request_id);
+        int? user_id = 0;
+        string? illustrate = "";
+        foreach (var r in record)
+        {
+            if (r.RequestId == request_id)
+            {
+                exist = true;
+                user_id = r.UserId;//获取申请人的ID
+                illustrate = r.Illustrate;//获取申请的内容
+                break;
+            }
+        }
+        if (exist == false)
+        {
+            code = 400;
+            msg = "认证申请不存在";
+            return BadRequest(new
+            {
+                code = code,
+                msg = msg,
+            });
+        }
+        var user = _database.Users.Where(x => x.UserId == user_id);
+        if (response == 2)
+        {
+            foreach (var r in record)
+            {
+                r.IsAccepted = 2;
+                await _database.SaveChangesAsync();
+            }
+            msg = "申请已拒绝";
+        }
+        else if (response == 1)
+        {
+            foreach (var r in record)
+            {
+                r.IsAccepted = 1;
+                await _database.SaveChangesAsync();
+            }
+            foreach (var u in user)
+            {
+                u.Professional = illustrate;
+                await _database.SaveChangesAsync();
+                msg="申请已通过";
+            }
+        }
+        else 
+        {
+            code=400;
+            msg="response参数错误";
+            return BadRequest(new
+            {
+                code = code,
+                msg = msg,
+            });
+        }
+        return Ok(new
+        {
+            code = code,
+            msg = msg,
+        });
+    }
 }
