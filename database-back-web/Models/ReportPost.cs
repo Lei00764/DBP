51c63b83a15396c2b5f6e19429faaca6ee820a32
Heelift <1091472606@qq.com>
2023-07-09 23:51:18 +0800

    完善了审核帖子页面，写了一个举报帖子的Model

---
+++ database-back-web/Models/ReportPost.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;  // for Key
+using System.ComponentModel.DataAnnotations.Schema;  // for Column
+
+namespace auth.Models;
+
+[Table("REPORTPOST")]
+[PrimaryKey(nameof(PostReportId),nameof(UserId),nameof(PostId),nameof(AdminId))]
+public class ReportPost
+{
+    // 请与数据库中表字段保持一致
+    [Column("POSTREPORTID")]
+    public int? PostReportId { get; set; }
+    [Column("USERID")]
+    public int? UserId { get; set; }
+    [Column("POSTID")]
+    public int? PostId { get; set; }
+    [Column("ADMINID")]
+    public int? AdminId { get; set; }
+    [Column("REASON")]
+    public string? Reason { get; set; }
+    [Column("TIME")]
+    public string? Time { get; set; }
+    [Column("RESULT")]
+    public string? Result { get; set; }
+}
