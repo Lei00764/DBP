0f73c4de2d339452b370317f609e2e86f31e363d
lei <xiang.lei.se@foxmail.com>
2023-09-09 13:55:02 +0800

    为部署做准备

--- database-front-web/src/main.js
+++ database-front-web/src/main.js
@@ -52,6 +52,7 @@ if (savedState) {
 // const { proxy } = getCurrentInstance();
 app.config.globalProperties.globalInfo = {
   avatarUrl: "http://localhost:5045/api/files/getAvatar/",
+  // avatarUrl: "http://124.220.110.93:5045/api/files/getAvatar/",
   search_keyword: "Search Key Words",
 }
 
