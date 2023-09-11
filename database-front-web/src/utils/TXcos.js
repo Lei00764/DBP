e5f937ec49a4643f6d23c911fb626610411fefe3
Xiang Lei <xiang.lei.se@foxmail.com>
2023-09-11 08:44:19 +0800

    Update TXcos.js

--- database-front-web/src/utils/TXcos.js
+++ database-front-web/src/utils/TXcos.js
@@ -2,8 +2,8 @@
 
 import COS from 'cos-js-sdk-v5'
 var cos = new COS({
-    SecretId: 'AKIDcAxfLvMl8VkmaARl05ziEXfeR1P0Ujur', // 推荐使用环境变量获取；用户的 SecretId，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参考https://cloud.tencent.com/document/product/598/37140
-    SecretKey: 'siM1a0ELkyzqY4ShrfHrKfrtnYggGsU2', // 推荐使用环境变量获取；用户的 SecretKey，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参考https://cloud.tencent.com/document/product/598/37140
+    SecretId: 'xx', // 推荐使用环境变量获取；用户的 SecretId，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参考https://cloud.tencent.com/document/product/598/37140
+    SecretKey: 'xx', // 推荐使用环境变量获取；用户的 SecretKey，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参考https://cloud.tencent.com/document/product/598/37140
 });
 
 
